c34d28d407745b2bd5b29c067e61ab92657347e3
osgofre <osgofre13@gmail.com>
2024-08-16 12:54:34 +0200

    Fix frontend

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('dist'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
